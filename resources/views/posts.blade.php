58650489b90f8c0bde17be482db4ea223894f443
Dan Wanjohi <danwwaititu@gmail.com>
2022-02-07 02:17:34 +0300

    Using the FileSystem Class to read a directory
    Making the code declarative, fetching all posts and adding metadata to posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,34 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My blog</title>
-    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-<article>
-    <h1><a href="/posts/my-first-post">My first Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus amet consectetur debitis doloribus error,
-        harum in ipsam iure molestiae nam nobis non nostrum numquam quam sint vel veritatis voluptas!
-    </p>
-</article>
-
-<article>
-    <h1><a href="/posts/my-second-post">My second Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt laborum nisi odio odit pariatur
-        quae quos similique soluta sunt! Architecto expedita, optio? Ad iste minima quae voluptas, voluptates
-        voluptatum.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium incidunt laborum nisi odio odit pariatur
-        quae quos similique soluta sunt! Architecto expedita, optio? Ad iste minima quae voluptas, voluptates
-        voluptatum.
-    </p>
-</article>
-
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
 </html>
