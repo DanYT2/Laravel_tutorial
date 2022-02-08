19f09cb91bc405560f4fdc7f42c3450c4d81bb56
Dan Wanjohi <danwwaititu@gmail.com>
2022-02-08 23:57:40 +0300

    Find a composer Package for Post Metadata
    Package: YamlFrontMatter
    Use of collections to get data

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    /*
+     * -------------------------
+     * Collection Approach
+     * -------------------------
+     * */
+    /* $posts = array_map(function ($file){
+        $document = YamlFrontMatter::parseFile($file);
+        return new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        );
+    }, $files);*/
 
     return view('posts', [
         'posts' => Post::all()
