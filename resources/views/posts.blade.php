98f6e01f5b8c293274b415eebfe3352a29c30e80
Dan Wanjohi <danwwaititu@gmail.com>
2022-02-12 12:32:29 +0300

    Layouts using extend and section

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,18 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My blog</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-    @foreach ($posts as $post)
-        <article class = '{{$loop->even ? 'mb-6' : ''}}'>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-            <div>
-                {{$post->excerpt}}
-            </div>
-        </article>
-    @endforeach
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+@foreach ($posts as $post)
+    <article class='{{$loop->even ? 'mb-6' : ''}}'>
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
+        </h1>
+        <div>
+            {{$post->excerpt}}
+        </div>
+    </article>
+@endforeach
+
+@endsection
+
