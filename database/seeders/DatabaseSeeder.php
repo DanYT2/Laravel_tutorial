97bd59e70be9799a7cf0cacb3065be374f6b1ad4
Dan Wanjohi <danwwaititu@gmail.com>
2022-02-15 19:18:37 +0300

    Database seeding to save time populating databases.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My family post',
+            'slug' => 'my-first-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => '<p>Body of my Family post</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work post',
+            'slug' => 'my-second-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => '<p>Body of my Work post</p>'
+        ]);
     }
 }
