4664f8742ed2c380ae586ee40c79f0aea5b43864
Dan Wanjohi <danwwaititu@gmail.com>
2022-02-17 17:20:11 +0300

    Eager load relationships on an existing model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,7 @@ class Post extends Model
     use HasFactory;
 //    protected $fillable = ['title', 'except', 'body'];
     protected $guarded = ['id'];
+    //protected $with = ['author', 'category'];
 
     /*public function getRouteKeyName(): string
     {
