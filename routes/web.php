5cef85c8fa0483966bda18d2e5bc80d71f458742
Dan Wanjohi <danwwaititu@gmail.com>
2022-03-06 13:14:19 +0300

    Search the messy way

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Models\Post;
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -17,8 +17,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $posts = Post::latest()->with('category', 'author');
+    if (request('search')) {
+        $posts
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all()
     ]);
 })->name('home');
@@ -32,16 +38,16 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 });
 
-Route::get('categories/{category:slug}', function (Category $category){
-    return view('posts',[
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
         'posts' => $category->posts->load(['category', 'author']),
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
 })->name('category');
 
-Route::get('authors/{author:username}', function (User $author){
-    return view('posts',[
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
         'posts' => $author->posts->load(['category', 'author']),
         'categories' => Category::all()
     ]);
