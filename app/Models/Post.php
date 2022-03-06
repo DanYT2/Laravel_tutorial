ee7cc22189d39e1522f50e44684e79f80f82dd04
Dan Wanjohi <danwwaititu@gmail.com>
2022-03-06 13:51:05 +0300

    Search the better way

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,24 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
 //    protected $fillable = ['title', 'except', 'body'];
-    protected $guarded = ['id'];
-    //protected $with = ['author', 'category'];
+  protected $guarded = ['id'];
+  protected $with = ['author', 'category'];
 
-    /*public function getRouteKeyName(): string
-    {
-        return 'slug';          Specifies what will be used as a search parameter in the routes file
-    }*/
-    public function category()
-    {
-        //hasOne, hasMany, belongsTo, belongsToMany
-        return $this->belongsTo(Category::class);
-    }
+  public function scopeFilter($query, array $filters)   // Post::newQuery()->filter()
+  {
+    $query->when($filters['search'] ?? false, function ($query, $search){
+      $query
+        ->where('title', 'like', '%' . $search . '%')
+        ->orWhere('body', 'like', '%' . $search . '%');
+    });
+  }
 
-    public function author()
-    {
-        return $this->belongsTo(User::class,'user_id');
-    }
+  /*public function getRouteKeyName(): string
+  {
+      return 'slug';          Specifies what will be used as a search parameter in the routes file
+  }*/
+  public function category()
+  {
+    //hasOne, hasMany, belongsTo, belongsToMany
+    return $this->belongsTo(Category::class);
+  }
+
+  public function author()
+  {
+    return $this->belongsTo(User::class, 'user_id');
+  }
 
 }
