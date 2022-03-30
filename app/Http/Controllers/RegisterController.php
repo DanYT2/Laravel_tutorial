2d974b68b5ce7861ed3400585dcdd3d3604608c6
Dan Wanjohi <danwwaititu@gmail.com>
2022-03-30 20:23:06 +0300

    Failed Validation and old input data

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,8 +17,8 @@ class RegisterController extends Controller
     //Create the user
     $attributes = request()->validate([
       'name' => 'required|max:255',
-      'username' => 'required|max:255|min:3',
-      'email' => 'required|email|max:255',
+      'username' => 'required|max:255|min:3|unique:users,username',
+      'email' => 'required|email|max:255|unique:users,email',
       'password' => 'required|max:255|min:8'
     ]);
     /*$attributes['password'] = bcrypt($attributes['password']);*/
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -8,15 +8,18 @@
           <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">
             Username
           </label>
-          <input type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
+          <input type="text" class="border border-gray-400 p-2 w-full" name="username" id="username" required value="{{ old('username') }}">
           <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">
             Name
           </label>
-          <input type="text" class="border border-gray-400 p-2 w-full" name="email" id="emil" required>
+          <input type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required value="{{ old('name') }}">
+          @error('name')
+          <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+          @enderror
           <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
             Email
           </label>
-          <input type="email" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
+          <input type="email" class="border border-gray-400 p-2 w-full" name="email" id="email" required value="{{ old('email') }}">
           <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
             Password
           </label>
