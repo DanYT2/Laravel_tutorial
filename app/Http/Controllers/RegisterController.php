cd2a43201e1ee89be4acb0a2b797fca71307bf95
Dan Wanjohi <danwwaititu@gmail.com>
2022-03-30 20:37:12 +0300

    Show a success flash message when a new user is created

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -24,5 +24,8 @@ class RegisterController extends Controller
     /*$attributes['password'] = bcrypt($attributes['password']);*/
 
     User::create($attributes);
+    session()->flash('success', 'Your account has been created.');
+
+    return redirect('/')->with('success', 'Your account has been created.');;
   }
 }
